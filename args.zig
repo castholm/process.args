53866548b3912175fa2772acd276df28c02b0f98
Carl Åstholm <carl@astholm.se>
2024-03-10 16:54:58 +0100

    `process.args.alloc/free` WASI

--- args.zig
+++ args.zig
@@ -48,11 +48,52 @@ fn freeSliceWindows(allocator: Allocator, args: []const [:0]const u8) void {
     _ = args;
 }
 
-fn allocSliceWasi(allocator: Allocator) Allocator.Error![][:0]const u8 {
-    _ = allocator;
+/// Memory layout: `args.*, args[0].*, args[1].*, ..., args[args.len - 1].*`
+fn allocSliceWasi(allocator: Allocator) Allocator.Error![][:0]u8 {
+    return allocSliceWasiInner(allocator) catch |err| switch (err) {
+        error.Unexpected => &.{},
+        error.OutOfMemory => error.OutOfMemory,
+    };
 }
 
+fn allocSliceWasiInner(allocator: Allocator) (Allocator.Error || os.UnexpectedError)![][:0]u8 {
+    var args_len: usize = undefined;
+    var buf_len: usize = undefined;
+    switch (os.wasi.args_sizes_get(&args_len, &buf_len)) {
+        .SUCCESS => {},
+        else => |err| return os.unexpectedErrno(err),
+    }
+    if (args_len == 0) return &.{};
+    const args_slice_bytes_len = @sizeOf([:0]u8) * args_len;
+    const args_many_bytes_len = @sizeOf([*:0]u8) * args_len;
+    const raw_len = args_slice_bytes_len + buf_len;
+
+    const raw = try allocator.alignedAlloc(u8, @alignOf([:0]u8), raw_len);
+    errdefer allocator.free(raw);
+
+    const args_slice_bytes_start = 0;
+    const args_slice = @as([*][:0]u8, @ptrCast(@alignCast(raw.ptr + args_slice_bytes_start)))[0..args_len];
+    const args_many_bytes_start = args_slice_bytes_len - args_many_bytes_len;
+    const args_many = @as([*][*:0]u8, @ptrCast(@alignCast(raw.ptr + args_many_bytes_start)))[0..args_len];
+    const buf_start = args_slice_bytes_len;
+    const buf = raw[buf_start..];
+    switch (os.wasi.args_get(args_many.ptr, buf.ptr)) {
+        .SUCCESS => {},
+        else => |err| return os.unexpectedErrno(err),
+    }
+    for (args_slice, args_many) |*dst, src| {
+        dst.* = mem.span(src);
+    }
+    return args_slice;
+}
+
+/// Memory layout: `args.*, args[0].*, args[1].*, ..., args[args.len - 1].*`
 fn freeSliceWasi(allocator: Allocator, args: []const [:0]const u8) void {
-    _ = allocator;
-    _ = args;
+    if (args.len == 0) return;
+    var raw_len: usize = @sizeOf([:0]u8) * args.len;
+    for (args) |arg| {
+        raw_len += arg.len + 1;
+    }
+    const raw = @as([*]align(@alignOf([:0]u8)) const u8, @ptrCast(args.ptr))[0..raw_len];
+    allocator.free(raw);
 }
